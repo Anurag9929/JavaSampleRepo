6cc88fe809c294578e6ecf0dfc74bb386e890703
Anurag Shukla <shuklaanu2902@gmail.com>
2021-11-23 17:22:38 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,19 @@
+package Packageone;
+
+
+public class sample{
+	public void one(){
+		int a=100;
+		int b=14;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public void samplee(int a){
+		int e=10;
+		int f=12;
+		System.out.println(e+f);
+	}
+
+	
+
+}
